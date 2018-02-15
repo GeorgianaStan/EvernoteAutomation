c12097fbfa480e992ba4d18d50ceb7efc4cbea54
gstan <gstan@P5RO-PORT727>
2018-02-15 10:56:12 +0200

    Adding search for note test

---
+++ EverNoteAutomation/Pages/SearchPage.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EverNoteAutomation.Pages
+{
+    public class SearchPage
+    {
+        public static string Title
+        {
+            get
+            {
+                var title = Driver.Instance.FindElement(By.CssSelector(".focus-NotesView-Note-noteTitle.qa-title")).Text;
+                return (title != null) ? title : String.Empty;
+            }
+        }
+
+        public static void GoTo()
+        {
+            LeftNavigation.SearchNote.Select();
+        }
+
+        public static SearchCommand SearchNote(string title)
+        {
+            return new SearchCommand(title);
+            
+        }
+    }
+
+    public class SearchCommand
+    {
+        private string title;
+
+        public SearchCommand(string title)
+        {
+            this.title = title;
+        }
+        public void SearchInNotebook()
+        {
+            var searchInput = Driver.Instance.FindElement(By.Id("gwt-debug-searchViewSearchBox"));
+            searchInput.SendKeys(title);
+            Driver.Wait(TimeSpan.FromSeconds(2));
+            searchInput.SendKeys(Keys.Enter);
+        }
+            
+    }
+}
---
+++ EverNoteTests/SmokeTests/SearchNoteTests.cs
@@ -0,0 +1,46 @@
+﻿using EverNoteAutomation;
+using EverNoteAutomation.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EverNoteTests.SmokeTests
+{
+    [TestFixture]
+    public class SearchNoteTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            Driver.Initialize();
+            LoginPage.GoTo();
+            LoginPage.LoginAs("georgi.pluralsight27@gmail.com").Continue().WithPassword("georgiana").Login();
+
+            //create a basic note
+            NewNotePage.GoTo();
+            NewNotePage.CreateNote("This is search note title").WithContent("Hi, this is the content").Done();
+        }
+
+        [Test]
+        public void SearchNote()
+        {
+            SearchPage.GoTo();
+            SearchPage.SearchNote("This is search note title").SearchInNotebook();
+
+            Assert.AreEqual(SearchPage.Title, "This is search note title", "Title did not match new note.");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            //delete the basic note created in the setup
+            NotesPage.TrashNote("This is search note title");
+
+            Driver.Close();
+        }
+    }
+}
